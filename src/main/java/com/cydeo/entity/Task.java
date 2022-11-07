4760ed882f98dea2dd6f705e90a633319fc49410
marklen <marklen86@gmail.com>
2022-11-07 02:37:49 -0600

    Task part created

---
+++ src/main/java/com/cydeo/entity/Task.java
@@ -0,0 +1,49 @@
+package com.cydeo.entity;
+
+import com.cydeo.dto.ProjectDTO;
+import com.cydeo.dto.UserDTO;
+import com.cydeo.enums.Status;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.Where;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import java.time.LocalDate;
+
+@Data
+@Entity
+@Table(name="tasks")
+@NoArgsConstructor
+@Where(clause = "is_deleted=false") //use only if is-deleted=false
+public class Task extends BaseEntity{
+
+    private String taskSubject;
+    private String taskDetail;
+    @Enumerated(EnumType.STRING)
+    private Status taskStatus;
+    @Column(columnDefinition = "DATE")
+    private LocalDate assignedDate;
+
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "project_id")
+    private Project project;
+
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "assigned_employee_id")
+    private User assignedEmployee;
+
+
+
+
+
+
+
+
+
+
+
+}
